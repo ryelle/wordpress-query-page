dd4d159da9b58d73628659db5e09b1f7464416d7
Kelly Dwan <kelly.dwan@gmail.com>
2016-11-26 00:28:32 -0500

    Add tests

---
+++ test/reducer.js
@@ -0,0 +1,105 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+import keyBy from 'lodash/keyBy';
+
+/**
+ * Internal dependencies
+ */
+import {
+	// action-types
+	PAGE_REQUEST,
+	PAGE_REQUEST_SUCCESS,
+	PAGE_REQUEST_FAILURE,
+	// reducers
+	items,
+	requests,
+	paths
+} from '../src/state';
+
+import pageSample from './fixtures/2';
+import pageAbout from './fixtures/1086';
+
+describe( 'Post reducer', () => {
+	describe( 'items', () => {
+		it( 'should have no change by default', () => {
+			const newState = items( undefined, {} );
+			expect( newState ).to.eql( {} );
+		} );
+
+		it( 'should store the new posts in state', () => {
+			const newState = items( undefined, { type: PAGE_REQUEST_SUCCESS, page: pageSample } );
+			const pagesById = keyBy( [ pageSample ], 'id' );
+			expect( newState ).to.eql( pagesById );
+		} );
+
+		it( 'should add new posts onto the existing post array', () => {
+			const originalState = deepFreeze( keyBy( [ pageSample ], 'id' ) );
+			const newState = items( originalState, { type: PAGE_REQUEST_SUCCESS, page: pageAbout } );
+			expect( newState ).to.eql( { ...originalState, 1086: pageAbout } );
+		} );
+	} );
+
+	describe( 'requests', () => {
+		it( 'should have no change by default', () => {
+			const newState = requests( undefined, {} );
+			expect( newState ).to.eql( {} );
+		} );
+
+		it( 'should track the requesting state of a new post', () => {
+			const newState = requests( undefined, { type: PAGE_REQUEST, pagePath: '/about' } );
+			expect( newState ).to.eql( { '/about': true } );
+		} );
+
+		it( 'should track the requesting state of successful post requests', () => {
+			const originalState = deepFreeze( { '/about': true } );
+			const newState = requests( originalState, { type: PAGE_REQUEST_SUCCESS, pagePath: '/about' } );
+			expect( newState ).to.eql( { '/about': false } );
+		} );
+
+		it( 'should track the requesting state of failed post requests', () => {
+			const originalState = deepFreeze( { '/about': true } );
+			const newState = requests( originalState, { type: PAGE_REQUEST_FAILURE, pagePath: '/about' } );
+			expect( newState ).to.eql( { '/about': false } );
+		} );
+
+		it( 'should track the requesting state of additional post requests', () => {
+			const originalState = deepFreeze( { '/about': true } );
+			const newState = requests( originalState, { type: PAGE_REQUEST, pagePath: '/sample-page' } );
+			expect( newState ).to.eql( { ...originalState, '/sample-page': true } );
+		} );
+	} );
+
+	describe( 'paths', () => {
+		it( 'should have no change by default', () => {
+			const newState = paths( undefined, {} );
+			expect( newState ).to.eql( {} );
+		} );
+
+		it( 'should track the post IDs for requested queries', () => {
+			const action = {
+				type: PAGE_REQUEST_SUCCESS,
+				pagePath: '/about',
+				postId: 1086,
+			};
+			const newState = paths( undefined, action );
+			expect( newState ).to.eql( { '/about': 1086 } );
+		} );
+
+		it( 'should track the post IDs for additional requested queries', () => {
+			const originalState = deepFreeze( { '/about': 1086 } );
+			const action = {
+				type: PAGE_REQUEST_SUCCESS,
+				pagePath: '/sample-page',
+				postId: 2,
+			};
+			const newState = paths( originalState, action );
+			expect( newState ).to.eql( {
+				'/about': 1086,
+				'/sample-page': 2
+			} );
+		} );
+	} );
+} );
---
+++ test/selectors.js
@@ -0,0 +1,76 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+import keyBy from 'lodash/keyBy';
+
+/**
+ * Internal dependencies
+ */
+import * as selectors from '../src/selectors';
+import pageSample from './fixtures/2';
+import pageAbout from './fixtures/1086';
+
+const pagesById = keyBy( [ pageSample, pageAbout ], 'id' );
+
+const state = deepFreeze( {
+	pages: {
+		items: pagesById,
+		requests: {
+			'/about': false,
+			'/sample-page': true,
+		},
+		paths: {
+			'/about': 1086,
+		}
+	}
+} );
+
+describe( 'Post selectors', function() {
+	it( 'should contain getPage method', function() {
+		expect( selectors.getPage ).to.be.a( 'function' );
+	} );
+
+	it( 'should contain isRequestingPage method', function() {
+		expect( selectors.isRequestingPage ).to.be.a( 'function' );
+	} );
+
+	it( 'should contain getPageIdFromPath method', function() {
+		expect( selectors.getPageIdFromPath ).to.be.a( 'function' );
+	} );
+
+	describe( 'isRequestingPage', function() {
+		it( 'Should get `false` if the page has not been requested yet', function() {
+			expect( selectors.isRequestingPage( state, '/unrequested-page' ) ).to.be.false;
+		} );
+
+		it( 'Should get `false` if this page has already been fetched', function() {
+			expect( selectors.isRequestingPage( state, '/about' ) ).to.be.false;
+		} );
+
+		it( 'Should get `true` if this page is being fetched', function() {
+			expect( selectors.isRequestingPage( state, '/sample-page' ) ).to.be.true;
+		} );
+	} );
+
+	describe( 'getPageIdFromPath', function() {
+		it( 'Should get `false` if the page has not been requested yet', function() {
+			expect( selectors.getPageIdFromPath( state, '/unrequested-page' ) ).to.be.false;
+		} );
+
+		it( 'Should get the page ID if this page is in our state', function() {
+			expect( selectors.getPageIdFromPath( state, '/about' ) ).to.eql( 1086 );
+		} );
+	} );
+
+	describe( 'getPage', function() {
+		it( 'Should get `undefined` if the post has not been requested yet', function() {
+			expect( selectors.getPage( state, 3 ) ).to.be.undefined;
+		} );
+
+		it( 'Should get the post object if this post is in our state', function() {
+			expect( selectors.getPage( state, 1086 ) ).to.eql( pageAbout );
+		} );
+	} );
+} );
